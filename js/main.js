b740d844c3ac8f6381248727264016de9d9644d4
AndreyKholopov <Andrey.Kholopov.Work@gmail.com>
2020-02-05 00:18:32 +0300

    add selection color and language

--- js/main.js
+++ js/main.js
@@ -2,13 +2,31 @@
 
 const category = document.querySelector('.header-menu');
 const categoryItem = document.querySelectorAll('.category-item');
+
 const homePage = document.querySelector('.header-top');
 const aboutPage = document.querySelector('.about');
 const portfolioPage = document.querySelector('.portfolio');
 const contactPage = document.querySelector('.contact');
 const headerPage = document.querySelector('.header');
+
 const aboutSkills = document.querySelector('.about-skills');
 const aboutSkillsCell = document.querySelectorAll('.about-skills__cell');
+
+const setingsButton = document.querySelector('.setings-button');
+const setingsWindow = document.querySelector('.setings-window');
+
+const checkboxLang = document.querySelector('.setings-lang__check');
+const contentEn = document.querySelectorAll('.content-EN');
+const contentRu = document.querySelectorAll('.content-RU');
+
+const checkboxLight = document.querySelector('.setings-light__check');
+const body = document.body;
+
+const headerTopInner = document.querySelector('.header-top__inner');
+const headerInner = document.querySelector('.header-inner');
+const aboutInner = document.querySelector('.about-inner');
+const portfolioInner = document.querySelector('.portfolio-inner');
+const contactInner = document.querySelector('.contact-inner');
 
 const homePageStyle = getComputedStyle(homePage);
 const aboutPageStyle = getComputedStyle(aboutPage);
@@ -44,6 +62,25 @@ const chooseSkillCell = (event) => {
    } else if (target.parentNode.classList.contains('about-skills__cell')) {
       aboutSkillsCell.forEach(item => item.classList.remove('active'));
       target.parentNode.classList.add('active');
+   }
+}
+
+const openSetings = (event) => {
+   event.preventDefault();
+   const target = event.target;
+
+   if (target.classList.contains('active')) {
+      target.classList.remove('active');
+      setingsWindow.style.transform = 'scaleX(0)';
+   } else if (target.parentNode.classList.contains('active')) {
+      target.parentNode.classList.remove('active');
+      setingsWindow.style.transform = 'scaleX(0)';
+   } else if (target.classList.contains('setings-button')) {
+      target.classList.add('active');
+      setingsWindow.style.transform = 'scaleX(1)';
+   } else if (target.parentNode.classList.contains('setings-button')) {
+      target.parentNode.classList.add('active');
+      setingsWindow.style.transform = 'scaleX(1)';
    }
 }
 
@@ -127,6 +164,79 @@ const displaySection = (item) => {
    }   
 }
 
+const changeLang = () => {
+   if (checkboxLang.checked) {
+      for (let i = 0; i <= contentRu.length - 1; i++) {
+         contentEn[i].style.display = 'inline-block';
+         contentRu[i].style.display = 'none';
+      }
+   } else {
+      for (let i = 0; i < contentRu.length; i++) {
+         contentEn[i].style.display = 'none';
+         contentRu[i].style.display = 'inline-block';
+      }
+   }
+}
+
+const changeBg = () => {
+   if (checkboxLight.checked) {
+      body.style.background = '#efefef';
+   } else {
+      body.style.background = '#2b3747';
+   }
+}
+
+const LightenDarkenColor = (col, amt) => {
+   let usePound = false;
+ 
+   if (col[0] == "#") {
+       col = col.slice(1);
+       usePound = true;
+   }
+
+   let num = parseInt(col,16);
+
+   let r = (num >> 16) + amt;
+
+   if (r > 255) r = 255;
+   else if  (r < 0) r = 0;
+
+   let b = ((num >> 8) & 0x00FF) + amt;
+
+   if (b > 255) b = 255;
+   else if  (b < 0) b = 0;
+
+   let g = (num & 0x0000FF) + amt;
+
+   if (g > 255) g = 255;
+   else if (g < 0) g = 0;
+
+   return (usePound?"#":"") + (g | (b << 8) | (r << 16)).toString(16);
+}
+
+const changeMainColor = (color) => {
+   let darkenColor = LightenDarkenColor(color, -37);
+   let darkenmoreColor = LightenDarkenColor(color, -64);
+
+   setingsButton.style.background = color;
+   setingsWindow.style.background = color;
+
+   headerTopInner.style.background = color;
+   headerTopInner.style.boxShadow = `6px 6px ${darkenColor}, 12px 12px ${darkenmoreColor}`;
+
+   headerInner.style.background = color;
+   headerInner.style.boxShadow = `6px 6px ${darkenColor}, 12px 12px ${darkenmoreColor}`;
+
+   aboutInner.style.background = color;
+   aboutInner.style.boxShadow = `6px 6px ${darkenColor}, 12px 12px ${darkenmoreColor}`;
+
+   portfolioInner.style.background = color;
+   portfolioInner.style.boxShadow = `6px 6px ${darkenColor}, 12px 12px ${darkenmoreColor}`;
+
+   contactInner.style.background = color;
+   contactInner.style.boxShadow = `6px 6px ${darkenColor}, 12px 12px ${darkenmoreColor}`;
+}
 
 category.addEventListener('click', chooseCategory);
 aboutSkills.addEventListener('click', chooseSkillCell);
+setingsButton.addEventListener('click', openSetings);
